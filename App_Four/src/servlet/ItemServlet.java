d9b677abdbcda0be98667f7d1f42e73e661a878c
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2022-12-27 22:17:02 +0530

    Created ItemServlet and overide get/post/pull/delete & option methods ...

---
+++ App_Four/src/servlet/ItemServlet.java
@@ -0,0 +1,44 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Java-EE_Application
+ * Date        : 12/27/2022
+ * Time        : 10:12 PM
+ * Year        : 2022
+ */
+
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/item")
+public class ItemServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
